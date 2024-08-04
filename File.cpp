a74a8dfbf2b0a14c798447e6cf4f9077900a45b0
BachiBachBach <thehessboi@gmail.com>
2024-08-03 21:26:31 -0500

    Initial commit

---
+++ File.cpp
@@ -0,0 +1,5 @@
+//
+// Created by thehe on 8/3/2024.
+//
+
+#include "File.h"
---
+++ File.h
@@ -0,0 +1,18 @@
+//
+// Created by thehe on 8/3/2024.
+//
+
+#ifndef TEXT_EDITOR_FILE_H
+#define TEXT_EDITOR_FILE_H
+
+#include <iostream>
+
+class File {
+private:
+    std::string name;
+    
+
+};
+
+
+#endif //TEXT_EDITOR_FILE_H
